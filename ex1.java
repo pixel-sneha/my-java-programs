ff5ed81a3816eb8d599bc0f531d9abff81bb8ab5
Sne <sweetsonu2006in@gmail.com>
2026-01-08 19:03:09 +0530

    Update ex1.java

--- ex1.java
+++ ex1.java
@@ -10,3 +10,31 @@ class Main {
         System.out.println("golden = " + golden);
     }
 }
+
+class Main {
+    public static void main(String[] args) {
+        char bi = 'b';
+        char dot = '.';
+        System.out.println("bi = " + bi);
+        System.out.println("dot = " + dot);
+    }
+}
+
+class Main {
+    public static void main(String[] args) {
+        boolean isJavaFun = true;
+        System.out.println("isJavaFun = " + isJavaFun);
+    }
+}
+
+class Main {
+    public static void main(String[] args) {
+        int k = 180;
+        float half = 0.5f;
+        char a = 'c';
+        System.out.println("k = " + k);
+        System.out.println("half = " + half);
+        System.out.println("a = " + a);
+    }
+}
+
