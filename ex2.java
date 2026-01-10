044f8f267d56736b1b84d5afb4be680fdd7a5351
Sne <sweetsonu2006in@gmail.com>
2026-01-10 19:30:48 +0530

    Create ex2.java

---
+++ ex2.java
@@ -0,0 +1,36 @@
+class Main {
+    public static void main(String[] args) {
+        int a = 3;
+        int b = 4;
+        int c = 5;
+        a += 3;
+        b -= a;
+        c *= a;
+        System.out.println("a = " + a);
+        System.out.println("b = " + b);
+        System.out.println("c = " + c);
+    }
+}
+
+class Main {
+    public static void main(String[] args) {
+        int n1 = 8;
+        int n2 = 9;
+        boolean b = n1>n2;
+        System.out.println("n1 = " + n1);
+        System.out.println("n2 = " + n2);
+        System.out.println("b = " + b);
+    }
+}
+
+
+//You are given a program, insert the missing values for the variables b1 and b2 so b3 will hold true.
+class Main {
+    public static void main(String[] args) {
+        boolean b1 = 5>2;
+        boolean b2 = 1!=3;
+        boolean b3 = b1 && b2;
+        System.out.println("b3 = " + b3);
+    }
+  
+}
