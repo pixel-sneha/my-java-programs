9d73ae588a20915c94d06be88d24dbf7efdf7f75
Sne <sweetsonu2006in@gmail.com>
2026-01-13 20:33:05 +0530

    Update ex3.java

--- ex3.java
+++ ex3.java
@@ -39,3 +39,40 @@ class Main {
         System.out.println(sum);
     }
 }
+
+//Write a program that:
+/*prints "Enter your age:" (with new line)
+Gets input from the user, one integer (the user's age)
+calculates the difference between the user's age and 120. For example: for age 32, 88 will be calculated.
+prints the calculated number. */
+
+import java.util.Scanner;
+
+class Main {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Enter your age:");
+        int age = scanner.nextInt();
+        int diff = 120-age;
+        System.out.println(diff);
+    }
+}
+
+//Write a program that prints "hello" 10 times. Do it using a for loop. 
+class Main {
+    public static void main(String[] args) {
+        for(int i=0;i<10;i++){
+            System.out.println("hello");
+        }
+    }
+}
+
+class Main {
+    public static void main(String[] args) {
+        int i = 1;
+        while(i<=10){
+            System.out.println("hello");
+            i++;
+        }
+    }
+}
