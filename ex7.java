7db259cda45131d4952bca1e2e9bd30b1aff2634
Sne <sweetsonu2006in@gmail.com>
2026-01-19 20:49:17 +0530

    Update ex7.java

--- ex7.java
+++ ex7.java
@@ -71,3 +71,29 @@ public class Main {
     }
 }
 
+/*Write a method named prod which gets an array of numbers 
+as argument and returns the product of all the numbers in the list.*/
+import java.util.Scanner;
+public class Main {
+    public static int prod(int[] arr) {
+        int product = 1;
+        for(int i = 0;i< arr.length;i++){
+            product *= arr[i];
+        }
+        return product;
+        
+    }
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        String text = scanner.nextLine();
+        String[] stringArr = text.split(",");
+        int[] arr = new int[stringArr.length];
+        for (int i = 0; i < stringArr.length; i++) {
+            arr[i] = Integer.parseInt(stringArr[i]);
+        }
+        
+        int result = prod(arr);
+        System.out.println("Product of array elements: " + result);
+    }
+}
