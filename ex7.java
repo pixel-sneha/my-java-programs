096b92922e82d5ce6a4fc25246d302d8b632084c
Sne <sweetsonu2006in@gmail.com>
2026-01-20 23:01:07 +0530

    Update ex7.java

--- ex7.java
+++ ex7.java
@@ -97,3 +97,33 @@ public class Main {
         System.out.println("Product of array elements: " + result);
     }
 }
+
+
+/*Write a method named reverse which gets an array of numbers as 
+argument and returns the reversed array.*/
+
+import java.util.Scanner;
+import java.util.Arrays;
+public class Main {
+    public static int[] reverse(int[] arr) {
+        int[] reversed = new int[arr.length];
+        for (int i = 0; i < arr.length; i++) {
+            reversed[i] = arr[arr.length - 1 - i];
+        }
+        
+        return reversed;
+    }
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        String text = scanner.nextLine();
+        String[] stringArr = text.split(",");
+        int[] arr = new int[stringArr.length];
+        for (int i = 0; i < stringArr.length; i++) {
+            arr[i] = Integer.parseInt(stringArr[i]);
+        }
+        
+        int[] result = reverse(arr);
+        System.out.println("The reversed array is: " + Arrays.toString(result));
+    }
+}
