4f02207ec49a7b2127662a7befb0ed2d627c5416
Sne <sweetsonu2006in@gmail.com>
2026-02-19 22:17:51 +0530

    Create ex15.java
    hashset exercises !

---
+++ ex15.java
@@ -0,0 +1,28 @@
+/*Create a method named addElement that takes two arguments:
+
+A HashSet of Strings (set)
+A String (element) to add
+The method should add the given element to the set and then print the updated set.*/
+import java.util.HashSet;
+import java.util.Scanner;
+import com.google.gson.Gson;
+import com.google.gson.reflect.TypeToken;
+import java.lang.reflect.Type;
+
+public class Main {
+    public static void addElement(HashSet<String> set, String element) {
+        set.add(element);
+        System.out.println(set);
+    }
+    
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        String setString = scanner.nextLine();
+        String element = scanner.nextLine();
+        
+        Type setType = new TypeToken<HashSet<String>>(){}.getType();
+        HashSet<String> set = new Gson().fromJson(setString, setType);
+        
+        addElement(set, element);
+    }
+}
