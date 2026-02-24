3b0e659da4ebcb76314ccb34aa2fafb9c8598b8b
Sne <sweetsonu2006in@gmail.com>
2026-02-24 20:06:46 +0530

    Update ex15.java

--- ex15.java
+++ ex15.java
@@ -55,3 +55,34 @@ public class Main {
         checkSet(set);
     }
 }
+
+/*Create a method named processSet that takes a HashSet of Strings as input. The method should:
+Clone the set,Clear the original set.
+Print the cloned set and its size,the original set (after clear) and its size.*/
+import java.util.HashSet;
+import java.util.Scanner;
+import com.google.gson.Gson;
+import com.google.gson.reflect.TypeToken;
+import java.lang.reflect.Type;
+@SuppressWarnings("unchecked")
+
+public class Main {
+    public static void processSet(HashSet<String> set) {
+        HashSet<String> clonedSet = (HashSet<String>) set.clone();
+        set.clear();
+        System.out.println("Cloned Set: " + clonedSet);
+        System.out.println("Cloned Set Size: " + clonedSet.size());
+        System.out.println("Original Set after clear: " +set);
+        System.out.println("Original Set Size: " +set.size());
+    }
+    
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        String setString = scanner.nextLine();
+        
+        Type setType = new TypeToken<HashSet<String>>(){}.getType();
+        HashSet<String> set = new Gson().fromJson(setString, setType);
+        
+        processSet(set);
+    }
+}
