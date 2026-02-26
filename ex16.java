b20c613e9809cfcf72ea123c7f5ca58f4e48e4ce
Sne <sweetsonu2006in@gmail.com>
2026-02-26 17:07:43 +0530

    Create ex16.java

---
+++ ex16.java
@@ -0,0 +1,24 @@
+/*Create a method named unionSets that takes
+ two HashSets of integers as input, computes their union, and prints it */
+import java.util.HashSet;
+import java.util.Scanner;
+import com.google.gson.Gson;
+import com.google.gson.reflect.TypeToken;
+import java.lang.reflect.Type;
+
+public class Main {
+    public static void unionSets(HashSet<Integer> set1, HashSet<Integer> set2) {
+        HashSet<Integer> unionSet = new HashSet<>(set1);
+        unionSet.addAll(set2);
+        System.out.println("Union: "+ unionSet);
+    }
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        String set1String = scanner.nextLine();
+        String set2String = scanner.nextLine();
+        Type setType = new TypeToken<HashSet<Integer>>(){}.getType();
+        HashSet<Integer> set1 = new Gson().fromJson(set1String, setType);
+        HashSet<Integer> set2 = new Gson().fromJson(set2String, setType);
+        unionSets(set1, set2);
+    }
+}
